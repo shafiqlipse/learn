84d1c6b628c2651fd7f1b2f1598299e110da1245
shafiq <ssekittoshafiq@gmail.com>
2024-12-18 17:04:22 +0300

    Reapply .gitignore rules

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 # Create your views here.
+@login_required(login_url='login')
 def dashboard(request):
     context = {}
     return render(request, "dashboard/overview.html", context)
